6b1f5d317b5eb74aca3425b1b36301955600b36e
ilyadery <ilyadrbn@gmail.com>
2020-12-14 12:27:58 +0200

    Добавьте файлы проекта.

---
+++ Lab 1/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Class1
+    {
+        public string nameDisc; 
+        public int countLek;       
+        public int countLR; 
+        public int countSR; 
+        int TimeLek;		
+        int TimeLR;  
+        int TimeSR;   
+
+        public Class1()
+        {
+        nameDisc = ""; 
+        countLek = 0;
+        countLR = 0;
+        countSR = 0;
+        }
+
+        public Class1(string name, int lek, int lr, int sr)
+        {
+            nameDisc = name; countLek = lek; countLR = lr; countSR = sr;
+        }
+
+        public int Sum()
+        {
+            return countLek * TimeLek + countLR * TimeLR + countSR * TimeSR;
+        }
+
+        public void setName()
+        {
+            
+            Console.Write("Введите название дисциплины: ");
+            nameDisc = Convert.ToString(Console.ReadLine());
+        }
+
+        public void setLek()
+        {
+          
+            Console.Write("Введите к-во лекций 1: ");
+            countLek = Convert.ToInt32(Console.ReadLine());
+        }
+   
+        public void setLR()
+        {
+          
+            Console.Write("Введите к-во лекций 2: ");
+            countLR = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void setSR()
+        {
+          
+            Console.Write("Введите к-во лекций 3: ");
+            countSR = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void setTimeLek()
+        {
+
+            Console.Write("Введите к-во часов 1: ");
+            TimeLek = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void setTimeLR()
+        {
+
+            Console.Write("Введите к-во часов 2: ");
+            TimeLR = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void setTimeSR()
+        {
+
+            Console.Write("Введите к-во часов 3: ");
+            TimeSR = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Lab 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 Disc1 = new Class1();
+
+            Disc1.setName();
+            Disc1.setTimeLek();
+            Disc1.setTimeLR();
+            Disc1.setTimeSR();
+            Disc1.setLek();
+            Disc1.setLR();
+            Disc1.setSR();
+
+            Console.WriteLine(" Общее число часов, отводимое на дисциплину " + Disc1.nameDisc + " равно " + Disc1.Sum());
+        }
+    }
+}
